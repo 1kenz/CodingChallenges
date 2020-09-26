3228d14df024b64682de6e6424e9a4412dd967c9
xkendx <63884610+xkendx@users.noreply.github.com>
2020-09-26 03:31:39 +0300

    Revert "feat: fibonacci"
    This reverts commit 925f7f684ac515d2e7c5d58b7fbae2aaeb6febfa.

--- JavaScript/JS-CC-001/index.js
+++ JavaScript/JS-CC-001/index.js
@@ -1,33 +1,13 @@
-document.getElementById("inp")
-    .addEventListener("keyup", function(event) {
-        event.preventDefault();
-        if (event.keyCode === 13) {
-            document.getElementById("btn").click();
-        }
-    });
-document.getElementById("btn").addEventListener("click", fibonacci);
-
-function fibonacci(number) {
-    var number = parseInt(document.getElementById("inp").value);
-    var a = 0;
-    var b = 1;
-    var c;
-    var x = 0;
-    if (number < 0) {
-        document.getElementById("res").innerHTML = `Please enter a positive number`;
-    } else if (number == 0) {
-        document.getElementById("res").innerHTML = `${number}. fibonacci number is 0`;
-    } else if (number == 1) {
-        document.getElementById("res").innerHTML = `${number}. fibonacci number is 1`;
-    } else {
-        while (x < (number - 1)) {
-            var c = a + b;
-            a = b;
-            b = c;
-            x++;
-        }
-        document.getElementById("res").innerHTML = `${number}. fibonacci number is ${c}`;
-        document.getElementById('inp').value = '';
-        document.getElementById('inp').focus();
-    }
+function fibonacci(x) {
+  let b = 1;
+  let a = 1;
+  let c;
+  while (x > 2) {
+    c = a + b;
+    a = b;
+    b = c;
+    x--;
+  }
+  return b;
 }
+console.log(fibonacci(16));

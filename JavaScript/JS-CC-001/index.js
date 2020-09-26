52774e5d812fc76ace846f67641a5091dc6828d4
xkendx <63884610+xkendx@users.noreply.github.com>
2020-09-26 04:32:24 +0300

    Update index.js

--- JavaScript/JS-CC-001/index.js
+++ JavaScript/JS-CC-001/index.js
@@ -14,20 +14,20 @@ function fibonacci(number) {
     var b = 1;
     var c;
     var x = 0;
-    if (number <= 1) {
-        document.getElementById("res").innerHTML = `Please enter higher than 1`;
+    if (number <= 0) {
+        document.getElementById("res").innerHTML = `Please enter positive number`;
         document.getElementById('inp').value = '';
         document.getElementById('inp').focus();
 //     } else if (number == 0) {
 //         document.getElementById("res").innerHTML = `${number}. fibonacci number is 0`;
 //         document.getElementById('inp').value = '';
 //         document.getElementById('inp').focus();
-//     } else if (number == 1) {
-//         document.getElementById("res").innerHTML = `${number}. fibonacci number is 1`;
-//         document.getElementById('inp').value = '';
-//         document.getElementById('inp').focus();
+    } else if (number == 1) {
+        document.getElementById("res").innerHTML = `${number}. fibonacci number is 1`;
+        document.getElementById('inp').value = '';
+        document.getElementById('inp').focus();
     } else {
-        while (x < (number - 1)) {
+        while (x < (number)) {
             var c = a + b;
             a = b;
             b = c;
